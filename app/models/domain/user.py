c85b3368e1fb864a035c256f17e021fe83b319c5
SeyedMohammadAli Noori <13noori79@gmail.com>
2023-07-24 21:07:34 +0430

    Add user domain models

---
+++ app/models/domain/user.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.infra.db.base import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    user_id: Mapped[int] = mapped_column(
+        primary_key=True,
+    )
+    username: Mapped[str] = mapped_column(String(255))
+    password: Mapped[str] = mapped_column(Text())
+    phone_number: Mapped[Optional[str]]
+    full_name: Mapped[str] = mapped_column(String(255))
