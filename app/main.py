8e0eafd675eea88473fc0cebf859f7814419fb5d
SeyedMohammadAli Noori <13noori79@gmail.com>
2023-07-25 11:29:07 +0430

    Add lifespan to app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.infra.db.database import Database
+from app.models.settings import Settings
+
+settings = Settings()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Database.init(
+        settings.DB_URI,
+    )
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 if __name__ == "__main__":
     uvicorn.run(
         "app:app",
