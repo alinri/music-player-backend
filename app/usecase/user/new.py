780db49b6f7999336191acfd47150fced31777ed
SeyedMohammadAli Noori <13noori79@gmail.com>
2023-07-25 11:01:54 +0430

    Add new user usecase

---
+++ app/usecase/user/new.py
@@ -0,0 +1,38 @@
+from fastapi import HTTPException, status
+from infra.auth.password.hash import HashPassword
+
+from app.infra.repo.interface.user import IUserRepo
+from app.models.domain.user import User
+from app.models.scheme.user.new_user import NewUserScheme
+
+
+class NewUserUsecase:
+    def __init__(
+        self,
+        user_repo: IUserRepo,
+    ) -> None:
+        self._user_repo = user_repo
+        self._hash_password = HashPassword()
+
+    def execute(
+        self,
+        new_user: NewUserScheme,
+    ):
+        username_exist = self._user_repo.get_by_username(
+            new_user.username,
+        )
+        if username_exist:
+            raise HTTPException(
+                status.HTTP_409_CONFLICT,
+                detail="نام کاربری تکراری",
+            )
+        hashed_password = self._hash_password.hash(
+            new_user.password,
+        )
+        user = User(
+            username=new_user.username,
+            passowrd=hashed_password,
+            phone_number=new_user.phone_number,
+            full_name=new_user.phone_number,
+        )
+        self._user_repo.insert_user(user)
