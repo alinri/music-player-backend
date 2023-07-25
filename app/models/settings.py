c2ac8d743db76df136a40e72950a7a062d69aaf0
SeyedMohammadAli Noori <13noori79@gmail.com>
2023-07-25 14:59:50 +0430

    Add SECRET_KEY to settings

--- app/models/settings.py
+++ app/models/settings.py
@@ -3,5 +3,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DB_URI: str
-
+    SECRET_KEY: str
+    ALGORITHM = "HS256"
     model_config = SettingsConfigDict(env_file=".env")
