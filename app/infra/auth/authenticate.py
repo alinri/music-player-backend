056684a20578b89af782e0a0984ce7002e847d4d
SeyedMohammadAli Noori <13noori79@gmail.com>
2023-07-25 15:32:13 +0430

    Add authenticate

---
+++ app/infra/auth/authenticate.py
@@ -0,0 +1,17 @@
+from typing import Annotated
+
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+
+from app.models.scheme.auth.token_data import TokenData
+
+from .token.verify import AccessTokenVerify
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+
+
+def authenticate(
+    token: Annotated[str, Depends(oauth2_scheme)],
+) -> TokenData:
+    access_token_verify = AccessTokenVerify()
+    access_token_verify.verify(token)
