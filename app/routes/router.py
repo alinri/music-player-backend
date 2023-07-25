7c8efc196a043bb8f917fd4b209354557cc87621
SeyedMohammadAli Noori <13noori79@gmail.com>
2023-07-25 17:45:53 +0430

    Add auth router

--- app/routes/router.py
+++ app/routes/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from . import user
+from . import auth, user
 
 router = APIRouter(prefix="/api")
 
 router.include_router(user.router)
+router.include_router(auth.router)
