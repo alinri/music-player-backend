cc2c6df9b87a097a0856bfb608fee818e2442ab8
SeyedMohammadAli Noori <13noori79@gmail.com>
2023-07-25 17:45:02 +0430

    Add type for algorithm in settings

--- app/models/settings.py
+++ app/models/settings.py
@@ -4,5 +4,5 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DB_URI: str
     SECRET_KEY: str
-    ALGORITHM = "HS256"
+    ALGORITHM: str = "HS256"
     model_config = SettingsConfigDict(env_file=".env")
