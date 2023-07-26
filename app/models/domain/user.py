a19d683cac7d651217d273bcd5e477a6fc3e8cc3
SeyedMohammadAli Noori <13noori79@gmail.com>
2023-07-26 23:02:18 +0430

    Add playlist domain model

--- app/models/domain/user.py
+++ app/models/domain/user.py
@@ -8,6 +8,7 @@ from app.infra.db.base import Base
 
 if TYPE_CHECKING:
     from .music import Music
+    from .playlist import PlayList
 
 
 class User(Base):
@@ -16,12 +17,27 @@ class User(Base):
     user_id: Mapped[int] = mapped_column(
         primary_key=True,
     )
-    username: Mapped[str] = mapped_column(String(255))
-    password: Mapped[str] = mapped_column(Text())
-    phone_number: Mapped[Optional[str]] = mapped_column(String(11))
-    full_name: Mapped[str] = mapped_column(String(255))
-    musics: Mapped[list["Music"]] = relationship(back_populates="user")
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
+    username: Mapped[str] = mapped_column(
+        String(255),
+    )
+    password: Mapped[str] = mapped_column(
+        Text(),
+    )
+    phone_number: Mapped[Optional[str]] = mapped_column(
+        String(11),
+    )
+    full_name: Mapped[str] = mapped_column(
+        String(255),
+    )
+    musics: Mapped[list["Music"]] = relationship(
+        back_populates="user",
+    )
+    playlists: Mapped[list["PlayList"]] = relationship(
+        back_populates="user",
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        default=datetime.now,
+    )
     modified_at: Mapped[datetime] = mapped_column(
         default=datetime.now,
         onupdate=datetime.now,
