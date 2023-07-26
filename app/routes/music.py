9f90163f1273e0193e10e6124f7f71525c0b0278
SeyedMohammadAli Noori <13noori79@gmail.com>
2023-07-26 20:51:30 +0430

    Add list_user_music router

--- app/routes/music.py
+++ app/routes/music.py
@@ -7,7 +7,9 @@ from app.infra.auth.authenticate import authenticate
 from app.infra.dependencies import db_session
 from app.infra.repo.sqlalchemy.music import MusicRepo
 from app.models.scheme.auth.token_data import TokenData
+from app.models.scheme.music.music_out import MusicOutScheme
 from app.models.scheme.music.new_music import NewMusicScheme
+from app.usecase.music.list import ListMusicUsecase
 from app.usecase.music.new import NewMusicUsecase
 
 router = APIRouter(prefix="/music", tags=["music"])
@@ -29,3 +31,18 @@ def login_user(
         new_music,
         token_data.user_id,
     )
+
+
+@router.get(
+    "/",
+    status_code=status.HTTP_200_OK,
+    response_model=list[MusicOutScheme],
+)
+def list_user_music(
+    token_data: Annotated[TokenData, Depends(authenticate)],
+    session: Annotated[Session, Depends(db_session)],
+):
+    list_music_usecase = ListMusicUsecase(
+        MusicRepo(session),
+    )
+    return list_music_usecase.execute(token_data.user_id)
