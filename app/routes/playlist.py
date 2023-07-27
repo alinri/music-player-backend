285ac299ae846af4cd7954fc3f9c2be4e557adcf
SeyedMohammadAli Noori <13noori79@gmail.com>
2023-07-27 23:43:40 +0430

    Add list playlist track endpoint

--- app/routes/playlist.py
+++ app/routes/playlist.py
@@ -6,11 +6,16 @@ from sqlalchemy.orm import Session
 from app.infra.auth.authenticate import authenticate
 from app.infra.dependencies import db_session
 from app.infra.repo.sqlalchemy.playlist import PlayListRepo
+from app.infra.repo.sqlalchemy.playlist_track import PlayListTrackRepo
 from app.models.scheme.auth.token_data import TokenData
 from app.models.scheme.playlist.new_playlist import NewPlayListScheme
 from app.models.scheme.playlist.playlist_out import PlayListOutScheme
+from app.models.scheme.playlist_track.playlist_track_out import (
+    PlayListTrackOutScheme,
+)
 from app.usecase.playlist.list import ListPlayListUsecase
 from app.usecase.playlist.new import NewPlayListUsecase
+from app.usecase.playlist_track.list import ListPlayListTrackUsecase
 
 router = APIRouter(prefix="/playlist", tags=["play list"])
 
@@ -48,3 +53,20 @@ def list_playlist(
     return list_playlist_usecase.execute(
         token_data.user_id,
     )
+
+
+@router.get(
+    "/{playlist_id}/tracks",
+    status_code=status.HTTP_200_OK,
+    dependencies=[Depends(authenticate)],
+    response_model=list[PlayListTrackOutScheme],
+)
+def list_playlist_tracks(
+    playlist_id: int,
+    session: Annotated[Session, Depends(db_session)],
+):
+    list_playlist_tracks_usecase = ListPlayListTrackUsecase(
+        PlayListTrackRepo(session),
+        PlayListRepo(session),
+    )
+    return list_playlist_tracks_usecase.execute(playlist_id)
