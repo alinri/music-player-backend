cd82d4cf7a5781065b0b2bfb09ec5fe8d2273c65
SeyedMohammadAli Noori <13noori79@gmail.com>
2023-07-27 22:58:08 +0430

    Add get_playlist_by_id to IPlayListRepo

--- app/infra/repo/interface/playlist.py
+++ app/infra/repo/interface/playlist.py
@@ -11,3 +11,10 @@ class IPlayListRepo(ABC):
     @abstractmethod
     def list_by_user_id(self, user_id: int) -> list[PlayList]:
         raise NotImplementedError
+
+    @abstractmethod
+    def get_playlist_by_id(
+        self,
+        playlist_id: int,
+    ) -> PlayList | None:
+        raise NotImplementedError
